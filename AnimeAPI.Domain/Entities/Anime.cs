12fdc3a7706fe56ab294e37d426c1dbbf3f23b3a
leandrosfarias <76781628+leandrosfarias@users.noreply.github.com>
2024-10-25 16:58:24 -0300

    feat: adicionar modelo de entidade Anime

---
+++ AnimeAPI.Domain/Entities/Anime.cs
@@ -0,0 +1,10 @@
+﻿namespace AnimeAPI.Domain.Entities;
+
+public class Anime
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Summary { get; set; }
+    public required string Director { get; set; }
+    public bool IsDeleted { get; set; }
+}
