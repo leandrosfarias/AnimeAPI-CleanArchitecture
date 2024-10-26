32ed868dc7f641b27fbbab9103bc5e799ff03bca
leandrosfarias <76781628+leandrosfarias@users.noreply.github.com>
2024-10-26 18:08:11 -0300

    feat: adicionar IAnimeService e implementação de AnimeService para lógica de negócios de Anime

---
+++ AnimeAPI.Application/Services/AnimeService.cs
@@ -0,0 +1,47 @@
+﻿using AnimeAPI.Domain.Entities;
+
+namespace AnimeAPI.Application.Services;
+
+public class AnimeService : IAnimeService
+{
+    public IAnimeRepository _animeRepository;
+
+    public AnimeService(IAnimeRepository animeRepository) => _animeRepository = animeRepository;
+
+    public async Task<Anime> AddAsync(Anime anime)
+    {
+        if (string.IsNullOrEmpty(anime.Name) || string.IsNullOrEmpty(anime.Director))
+        {
+            throw new ArgumentException("Nome e Diretor são obrigatorios");
+        }
+        return await _animeRepository.AddAsync(anime);
+    }
+
+    public async Task<IEnumerable<Anime>> GetAllAsync(string? director = null, string? name = null, string? summaryKeyword = null, int pageIndex = 1, int pageSize = 10)
+    {
+        return await _animeRepository.GetAllAsync(director, name, summaryKeyword, pageIndex, pageSize);
+    }
+
+    public async Task UpdateAsync(Anime anime)
+    {
+        if (anime.Id <= 0)
+        {
+            throw new ArgumentException("ID inválido.");
+        }
+        await _animeRepository.UpdateAsync(anime);
+    }
+
+    public async Task DeleteByIdAsync(int id)
+    {
+        if (id <= 0)
+        {
+            throw new ArgumentException("ID inválido.");
+        }
+        await _animeRepository.DeleteByIdAsync(id);
+    }
+
+    public Task<Anime> GetByIdAsync(int id)
+    {
+        return _animeRepository.GetById(id);
+    }
+}
---
+++ AnimeAPI.Application/Services/IAnimeService.cs
@@ -0,0 +1,11 @@
+﻿using AnimeAPI.Domain.Entities;
+
+public interface IAnimeService
+{
+    Task<Anime> AddAsync(Anime anime);
+    Task<Anime> GetByIdAsync(int id);
+    Task<IEnumerable<Anime>> GetAllAsync(string? director = null, string? name = null, string? summaryKeyword = null, int pageIndex = 1, int pageSize = 10);
+    Task UpdateAsync(Anime anime);
+    Task DeleteByIdAsync(int id);
+}
+
