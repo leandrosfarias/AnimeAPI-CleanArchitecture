5e0b9bfd51f77edfcca60dcc08819384649eb2e5
leandrosfarias <76781628+leandrosfarias@users.noreply.github.com>
2024-10-26 18:08:42 -0300

    feat: adicionar AnimeController para expor operações CRUD via API

---
+++ AnimeAPI.API/Controllers/AnimeController.cs
@@ -0,0 +1,59 @@
+﻿using AnimeAPI.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimeAPI.API.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class AnimeController : ControllerBase
+{
+    private readonly IAnimeService _animeService;
+
+    public AnimeController(IAnimeService animeService) => _animeService = animeService;
+
+    // POST: api/animes
+    [HttpPost]
+    public async Task<IActionResult> Create(Anime anime)
+    {
+        anime.Id = 0;
+        var createdAnime = await _animeService.AddAsync(anime);
+        return CreatedAtAction(nameof(GetById), new { id = createdAnime.Id }, createdAnime);
+    }
+
+    // GET: api/animes/{id}
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var anime = await _animeService.GetByIdAsync(id);
+
+        if (anime == null)
+        {
+            return NotFound();
+        }
+        return Ok(anime);
+    }
+
+    // GET: api/animes
+    [HttpGet]
+    public async Task<IActionResult> GetAll([FromQuery] string? director, [FromQuery] string? name, [FromQuery] string? summaryKeyword, int pageIndex = 1, int pageSize = 10)
+    {
+        var animes = await _animeService.GetAllAsync(director, name, summaryKeyword, pageIndex, pageSize);
+        return Ok(animes);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(int id, Anime anime)
+    {
+        anime.Id = id;
+        await _animeService.UpdateAsync(anime);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await _animeService.DeleteByIdAsync(id);
+        return NoContent();
+    }
+}
+
