a6cd1ca3f740d7cf2b0d0dec85219effc7861280
leandrosfarias <76781628+leandrosfarias@users.noreply.github.com>
2024-10-27 07:16:31 -0300

    feat: adicionar middleware de autenticação via chave de API

---
+++ AnimeAPI.API/Middleware/ApiKeyMiddleware .cs
@@ -0,0 +1,36 @@
+﻿namespace AnimeAPI.API.Middleware;
+
+public class ApiKeyMiddleware
+{
+    private readonly RequestDelegate _next;
+    private const string APIKEYNAME = "x-api-key";
+
+    public ApiKeyMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (!context.Request.Headers.TryGetValue(APIKEYNAME, out var extractedApiKey))
+        {
+            context.Response.StatusCode = 401;
+            await context.Response.WriteAsync("API Key is missing");
+            return;
+        }
+
+        // Carregar a chave de API do .env
+        var apiKey = Environment.GetEnvironmentVariable("API_KEY");
+
+        if (!apiKey!.Equals(extractedApiKey))
+        {
+            context.Response.StatusCode = 401;
+            await context.Response.WriteAsync("Unauthorized client");
+            return;
+        }
+
+        await _next(context);
+    }
+}
+
+
